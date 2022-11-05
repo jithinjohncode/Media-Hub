1abc81b86c36f127f574a97b4d140e589769ad55
jithin john <jithinjohncode@gmail.com>
2022-11-05 05:29:38 +0000

    Update Home.tsx

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import img from '../../images/commingsoon.png'
+import React from "react";
+import img from "../../images/commingsoon.png";
 
 function Home() {
-    return (
-        <div className="w-full h-full bg-[#F2EBE1]">
-            <h1>Comeing soon....</h1>
-        </div>
-    );
+  return (
+    <div className="w-full h-full bg-[#F2EBE1]">
+      <h1>Coming soon....</h1>
+    </div>
+  );
 }
 
 export default Home;
